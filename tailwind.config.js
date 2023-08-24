0ca8764a9262a301d6ffab370b0cb1414a5658d6
Fabio Nascimento <fabionascimento@smartbr.com>
2023-08-24 07:46:19 -0300

    feat: update color default

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1036BA",
+        primaryHover: "#629900",
+        secondary: "#C1FF00",
+        terciary: "#34C759",
+        tinted: "#CED9FF",
+        border: "#ccc",
+        texts: "#333d4f",
+        white: "#EDEBF5",
+        black: "#0A0D27",
+        lightGray: "#FAFAFA",
+        gray: "#B5B5B5",
+        darkGray: "#767676",
+        danger: "#cf0404",
+        success: "#3acc15",
+      },
+    },
   },
   plugins: [],
 };
